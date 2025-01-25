6aa5d56efcd9aab6d48f11313a35ac5eb2aa155d
mtouzot <martin.touzot@gmail.com>
2025-01-25 10:58:10 +0100

    Update Read the Docs options and add a svg option

--- docs/conf.py
+++ docs/conf.py
@@ -57,10 +57,29 @@ exclude_patterns = []
 #
 html_theme = "sphinx_rtd_theme"
 
+html_theme_options = {
+    "logo_only": False,
+    "prev_next_buttons_location": "bottom",
+    "style_external_links": True,
+    "vcs_pageview_mode": "raw",
+    "style_nav_header_background": "#2980B9",
+    "flyout_display": "hidden",
+    "version_selector": True,
+    "language_selector": True,
+    # Toc options
+    "collapse_navigation": True,
+    "sticky_navigation": True,
+    "navigation_depth": 4,
+    "includehidden": False,
+    "titles_only": False,
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+html_logo = "_static/icon/logo.svg"
 
 # A favicon
 favicons = [
